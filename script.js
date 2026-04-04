ab689157eff40c92012139c50fcf2fdccb4368d4
emmanuel002332 <emmanuelnnam002@gmail.com>
2026-04-04 08:12:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// FAQ Accordion Functionality
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    const answer = item.querySelector('.faq-answer');
+    
+    question.addEventListener('click', () => {
+        // Toggle answer visibility
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+});
